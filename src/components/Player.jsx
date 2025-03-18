088390b8455645ef11a5df23b4bb2cb175193b9f
nikosemp <nikosemp@hotmail.com>
2025-03-19 01:41:05 +0200

    sanitize

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,27 +1,36 @@
 import { useState } from "react"
+import DOMPurify from 'dompurify';
 
 export default function Player({initialName, symbol, isActive, onNameChange}){
     const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
-        /* setIsEditing((editing) => !editing); */
         if (!isEditing || (isEditing && playerName.trim() !== '')) {
             setIsEditing((editing) => !editing);
         }
         if (isEditing && playerName.trim() !== '') {
-                onNameChange(symbol, playerName);
+            onNameChange(symbol, playerName);
         }
     }
 
     function handleChange(evt){
-        setPlayerName(evt.target.value);
+        const newValue = evt.target.value.replace(/[<>]/g, '');
+        if (newValue.length <= 15) {
+            setPlayerName(newValue);
+        }
     }
 
-    let editablePlayerName = <span className='player-name'>{playerName}</span>;
+    let editablePlayerName = <span className='player-name'>{playerName.replace(/[<>]/g, '')}</span>;
 
     if(isEditing){
-        editablePlayerName = <input type='text' required value={playerName} onChange={handleChange}/>
+        editablePlayerName = <input 
+            type='text' 
+            required 
+            value={playerName} 
+            onChange={handleChange} 
+            maxLength={15}
+        />
     }
 
     const isSaveDisabled = isEditing && playerName.trim() === '';
